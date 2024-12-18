aa3bc9c6c5b687f519152269188318fb4767d0ab
vic-dti <victor.yano@dtidigital.com.br>
2024-12-18 17:58:38 -0300

    Commit 5 - Move Function

--- index.js
+++ index.js
@@ -1,62 +1,61 @@
 const { readFileSync } = require('fs');
 
+function formatarMoeda(valor) {
+    return new Intl.NumberFormat("pt-BR",
+        { style: "currency", currency: "BRL",
+          minimumFractionDigits: 2 }).format(valor / 100);
+}
+
+function getPeca(pecas, apresentacao) {
+    return pecas[apresentacao.id];
+}
+
+function calcularTotalApresentacao(pecas, apre) {
+    let total = 0;
+    const peca = getPeca(pecas, apre);
+    switch (peca.tipo) {
+        case "tragedia":
+            total = 40000;
+            if (apre.audiencia > 30) {
+                total += 1000 * (apre.audiencia - 30);
+            }
+            break;
+        case "comedia":
+            total = 30000;
+            if (apre.audiencia > 20) {
+                total += 10000 + 500 * (apre.audiencia - 20);
+            }
+            total += 300 * apre.audiencia;
+            break;
+        default:
+            throw new Error(`Peça desconhecia: ${peca.tipo}`);
+    }
+    return total;
+}
+
+function calcularCredito(pecas, apre) {
+    let creditos = 0;
+    creditos += Math.max(apre.audiencia - 30, 0);
+    if (getPeca(pecas, apre).tipo === "comedia") 
+        creditos += Math.floor(apre.audiencia / 5);
+    return creditos;   
+}
+
+function calcularTotalFatura(pecas, apresentacoes) {
+    return apresentacoes.reduce((total, apre) => total + calcularTotalApresentacao(pecas, apre), 0);
+}
+
+function calcularTotalCreditos(pecas, apresentacoes) {
+    return apresentacoes.reduce((total, apre) => total + calcularCredito(pecas, apre), 0);
+}
+
 function gerarFaturaStr(fatura, pecas) {
-
-    function getPeca(apresentacao) {
-        return pecas[apresentacao.id];
-    }
-
-    function calcularTotalApresentacao(apre) {
-        let total = 0;
-        const peca = getPeca(apre);
-        switch (peca.tipo) {
-            case "tragedia":
-                total = 40000;
-                if (apre.audiencia > 30) {
-                    total += 1000 * (apre.audiencia - 30);
-                }
-                break;
-            case "comedia":
-                total = 30000;
-                if (apre.audiencia > 20) {
-                    total += 10000 + 500 * (apre.audiencia - 20);
-                }
-                total += 300 * apre.audiencia;
-                break;
-            default:
-                throw new Error(`Peça desconhecia: ${peca.tipo}`);
-        }
-        return total;
-    }
-
-    function calcularCredito(apre) {
-        let creditos = 0;
-        creditos += Math.max(apre.audiencia - 30, 0);
-        if (getPeca(apre).tipo === "comedia") 
-            creditos += Math.floor(apre.audiencia / 5);
-        return creditos;   
-    }
-
-    function formatarMoeda(valor) {
-        return new Intl.NumberFormat("pt-BR",
-            { style: "currency", currency: "BRL",
-              minimumFractionDigits: 2 }).format(valor / 100);
-    }
-
-    function calcularTotalFatura() {
-        return fatura.apresentacoes.reduce((total, apre) => total + calcularTotalApresentacao(apre), 0);
-    }
-
-    function calcularTotalCreditos() {
-        return fatura.apresentacoes.reduce((total, apre) => total + calcularCredito(apre), 0);
-    }
-
     let faturaStr = `Fatura ${fatura.cliente}\n`;
     for (let apre of fatura.apresentacoes) {
-        faturaStr += `  ${getPeca(apre).nome}: ${formatarMoeda(calcularTotalApresentacao(apre))} (${apre.audiencia} assentos)\n`;
+        faturaStr += `  ${getPeca(pecas, apre).nome}: ${formatarMoeda(calcularTotalApresentacao(pecas, apre))} (${apre.audiencia} assentos)\n`;
     }
-    faturaStr += `Valor total: ${formatarMoeda(calcularTotalFatura())}\n`;
-    faturaStr += `Créditos acumulados: ${calcularTotalCreditos()} \n`;
+    faturaStr += `Valor total: ${formatarMoeda(calcularTotalFatura(pecas, fatura.apresentacoes))}\n`;
+    faturaStr += `Créditos acumulados: ${calcularTotalCreditos(pecas, fatura.apresentacoes)} \n`;
     return faturaStr;
 }
 
